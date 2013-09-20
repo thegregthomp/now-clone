e587a8836a154dd8b5dc24a92fcd09e772ec3a95
Greg Thompson <greg@themightyengine.com>
2013-09-20 14:33:09 -0400

    init17

--- config/routes.js
+++ config/routes.js
@@ -13,37 +13,11 @@ module.exports = function(app, Instagram, io) {
 	});
 	app.post('/subscribe', function(req, res){
 			//io.sockets.emit('alert', "PICTURE~");
-			req.body.forEach(function(notificationOjb){
-			    // Every notification object contains the id of the geography
-			    // that has been updated, and the photo can be obtained from
-			    // that geography
-			    https.get({
-			      host: 'api.instagram.com',
-			      path: '/v1/geographies/' + notificationOjb.object_id + '/media/recent' +
-			      '?' + querystring.stringify({client_id: process.env.instagram_client_id,count: 1}),
-			    }, function(res){
-			      var raw = "";
-
-			      res.on('data', function(chunk) {
-			        raw += chunk;
-			      });
-
-			      // When the whole body has arrived, it has to be a valid JSON, with data,
-			      // and the first photo of the date must to have a location attribute.
-			      // If so, the photo is emitted through the websocket
-			      res.on('end', function() {
-			        var response = JSON.parse(raw);
-			        if(response['data'].length > 0 && response['data'][0]['location'] != null) {
-			          io.sockets.emit('photo', raw);
-			        } else {
-			          console.log("ERROR: %s", util.inspect(response['meta']));
-			        }
-			      });
-
-			    });
+			req.body.forEach(function(data){
+			     io.sockets.emit('photo', raw);
+			     io.sockets.emit('alert', "PICTURE~kk");
 			  });
 
-			  response.writeHead(200);
 	});
 	
 }
