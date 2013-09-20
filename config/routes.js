efa9aecfa74c1a701e37d6f43327b6e7dd1cd6ab
Greg Thompson <greg@themightyengine.com>
2013-09-20 16:19:49 -0400

    init43

--- config/routes.js
+++ config/routes.js
@@ -17,7 +17,7 @@ module.exports = function(app, Instagram, io, getPhoto) {
 			    body += chunk;
 			  });
 			  req.on('end', function () {
-			    body = JSON.parse(body);
+			    body = JSON.parse(body[0].object_id);
 
 			    getPhoto(body);
 			    res.writeHead(200);
