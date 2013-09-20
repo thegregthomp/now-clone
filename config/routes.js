fa6332a8af98540ac9d00026522d89243241cd05
Greg Thompson <greg@themightyengine.com>
2013-09-20 14:54:57 -0400

    init22

--- config/routes.js
+++ config/routes.js
@@ -12,9 +12,21 @@ module.exports = function(app, Instagram, io) {
 			Instagram.subscriptions.handshake(req, res); 
 	});
 	app.post('/subscribe', function(req, res){
-			
+			console.log("------------------------------------------");
+			console.log("------------------------------------------");
+			console.log("------------------------------------------");
 			console.log(req);
-
+			console.log("------------------------------------------");
+			console.log("------------------------------------------");
+			console.log("------------------------------------------");
+			console.log(res);
+			console.log("------------------------------------------");
+			console.log("------------------------------------------");
+			console.log("------------------------------------------");
+			console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			console.log("");
+			console.log("");
+ 			io.sockets.emit('photo', "PHOTO");
 
 			res.writeHead(200);
 			res.end();
