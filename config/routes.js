6ddd1b3e597f8d14b4f59e2ff60e3b617a3da984
Greg Thompson <greg@themightyengine.com>
2013-09-21 08:58:22 -0400

    init56

--- config/routes.js
+++ config/routes.js
@@ -23,16 +23,18 @@ module.exports = function(app, Instagram, io, getPhoto) {
 			  });
 	});
 	app.get('/set_sub', function(req, res){
-			Instagram.subscriptions.subscribe({ object: 'tag', object_id: 'fun' });
+			//Instagram.subscriptions.subscribe({ object: 'tag', object_id: 'fun' });
+			Instagram.media.subscribe({ lat: 39.952565, lng: -75.165428, radius: 5000 });
 			res.writeHead(200);
 			res.end();
 	});
-	app.get('/poop', function(req, res){
-			Instagram.subscriptions.unsubscribe_all();
+	app.get('/cancel_sub', function(req, res){
+			//curl -X DELETE https://api.instagram.com/v1/subscriptions\?client_secret\=8f06292e11d742c2b453d1c5661469df\&object\=tag\&client_id\=9425e6b8836d420091f6d8ae5f121200
+			//Instagram.subscriptions.unsubscribe_all();
 			res.writeHead(200);
 			res.end();
 	});
-	
+
 	
 	
 }
