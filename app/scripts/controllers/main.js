9fd606974229ab27902eb221d15103035106bca4
Greg Thompson <greg@themightyengine.com>
2013-09-23 13:39:22 -0400

    init56

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,7 @@ var MainCtrl = function ($scope, socket) {
   var $container = $('#holder');
   $scope.$on('$viewContentLoaded', function() {
     $( function() {
-      //$container.packery();
+      $container.packery();
     });
   });
 
@@ -28,8 +28,11 @@ var MainCtrl = function ($scope, socket) {
    
     var items = "<img src = '"+data+"' class = 'item'/>";
     var $items = $( items );
-    $container.prepend( $items ).fadeIn('slow');
-    //$container.prepend( $items ).packery( 'prepended', $items );
+    $container.prepend( $items ).packery( 'prepended', $items );
+    //$container.packery( 'appended', img );
+
+    //$('.holder').prepended(element);
+    //$scope.socketnumber = data.hello;
     });
 };
 
